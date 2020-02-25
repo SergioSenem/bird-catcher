f24ba00b8cdf9ea4dc7ec57328b4f660c542e6b8
Sergio E. Senem <sergio.e.senem@gmail.com>
2020-02-25 20:00:45 -0300

    Problemas na tela de listagem de hashtags

--- db/migrate/20200216200440_create_hashtag_users.rb
+++ db/migrate/20200216200440_create_hashtag_users.rb
@@ -1,6 +1,6 @@
-class CreateHashtagsUsers < ActiveRecord::Migration[6.0]
+class CreateHashtagUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :hashtags_users do |t|
+    create_table :hashtag_users do |t|
       t.integer :user_id
       t.integer :hashtag_id
 
