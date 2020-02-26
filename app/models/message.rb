9cc3fa65198be654341478c7f42e7d02d5816d68
Sergio E. Senem <sergio.e.senem@gmail.com>
2020-02-26 20:38:45 -0300

    Ajustando métodos de busca de mensagens

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,7 +7,19 @@ class Message < ApplicationRecord
 
 	require 'twitter'
 
-	def get_twitter_client
+	def self.get_messages_from_twitter
+		tweets = get_tweets
+		save_tweets(tweets)
+	end
+
+	def self.get_hashtag_messages_from_twitter(hashtag)
+		client = get_twitter_client
+		tweets = get_tweets_by_hashtag(hashtag.value)
+		save_hashtag_tweets(hashtag.id, tweets)
+	end
+
+	private
+	def self.get_twitter_client
 		Twitter::REST::Client.new do |config|
 	    	config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
 	    	config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
@@ -16,18 +28,7 @@ class Message < ApplicationRecord
 		end
 	end
 
-	def get_messages_from_twitter
-		tweets = get_tweets
-		save_tweets(tweets)
-	end
-
-	def get_hashtag_messages_from_twitter(hashtag)
-		client = get_twitter_client
-		tweets = get_tweets_by_hashtag(hashtag.value)
-		save_hashtag_tweets(hashtag.id, tweets)
-	end
-
-	def get_tweets
+	def self.get_tweets
   		client = get_twitter_client
 
 		hashtags = Hashtag.all
@@ -40,12 +41,12 @@ class Message < ApplicationRecord
 		tweets
   	end
 
-  	def get_tweets_by_hashtag(client, hashtag)
+  	def self.get_tweets_by_hashtag(client, hashtag)
   		tweets = client.search("##{hashtag}").take(100)
   		remove_existing(tweets)
   	end
 
-  	def remove_existing(tweets)
+  	def self.remove_existing(tweets)
   		messages = Message.where(twitter_id: tweets.map(&:id))
 
   		new_tweets = Array.new
@@ -60,20 +61,20 @@ class Message < ApplicationRecord
   		return new_tweets
   	end
 
-  	def save_tweets(messages)
+  	def self.save_tweets(messages)
   		messages.each do |hashtag, tweets|
   			save_hashtag_tweets(hashtag, tweets)
 		end
   	end
 
-  	def save_hashtag_tweets(hashtag_id, tweets)
+  	def self.save_hashtag_tweets(hashtag_id, tweets)
   		tweets.each do |tweet|	
 			message = save_message_from_tweet(tweet)
 			save_hashtag_message(hashtag, message.id)
 		end
   	end
 
-  	def save_message_from_tweet(tweet)
+  	def self.save_message_from_tweet(tweet)
   		message = Message.new
 		message.twitter_id = tweet.id
 		message.author = tweet.user.screen_name
@@ -85,7 +86,7 @@ class Message < ApplicationRecord
 		message
   	end
 
-  	def save_hashtag_message(hashtag_id, message_id)
+  	def self.save_hashtag_message(hashtag_id, message_id)
   		hashtag_message = HashtagMessage.new
 		hashtag_message.hashtag_id = hashtag
 		hashtag_message.message_id = message.id
