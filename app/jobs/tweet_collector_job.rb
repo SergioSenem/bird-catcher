736be55ea885cfc0b60c866a99fcdf509d80d8b2
Sergio E. Senem <sergio.e.senem@gmail.com>
2020-02-26 20:27:23 -0300

    Criando variaveis de ambiente e refatorando metodos de busca de mensagens

--- app/jobs/tweet_collector_job.rb
+++ app/jobs/tweet_collector_job.rb
@@ -1,76 +1,7 @@
 class TweetCollectorJob < ApplicationJob
-  queue_as :default
+  	queue_as :default
 
-  require 'twitter'
-
-  def perform
-    tweets = get_tweets
-
-	save_tweets(tweets)
-  end
-
-  def perform(hashtag)
-  	tweets = get_tweets(hashtag)
-
-  	save_tweets
-  end
-
-  def get_tweets(hashtag = nil)
-  	client = Twitter::REST::Client.new do |config|
-	    config.consumer_key        = "FZNGD6vxr9Nu12gY3eW2H9ESw"
-	    config.consumer_secret     = "cvqlxT79AFw90aJoFkTg5qk9HeY8aHte9ZND4Ie5GhRQb2WLrp"
-	    config.access_token        = "1225205197829496838-XZvxdxMoaestlD36U7YVH0yPQFBNG7"
-	    config.access_token_secret = "kgsBOK2FtXz3bDXw3NBIQxjXOJmgLlB6J2knhHumVGuNX"
-	end
-
-	if hashtag.nil?
-		hashtags = Hashtag.all
-	else
-		hashtags = Hashtag.find_by_value(hashtag)
-	end
-
-	tweets = Hash.new
-
-	hashtags.each do |hashtag|
-		tweets[hashtag.id] = client.search("##{hashtag.value}").take(100)
-		tweets[hashtag.id] = remove_existing(tweets[hashtag.id])
-	end
-
-	tweets
-  end
-
-  def remove_existing(tweets)
-  	messages = Message.where(twitter_id: tweets.map(&:id))
-
-  	new_tweets = Array.new
-  	existing_tweets_ids = messages.map(&:twitter_id)
-
-  	tweets.each do |tweet|
-  		unless existing_tweets_ids.include?(tweet.id)
-  			new_tweets.push(tweet)
-  		end
+  	def perform
+    	Message.get_messages_from_twitter
   	end
-
-  	return new_tweets
-  end
-
-  def save_tweets(messages)
-  	messages.each do |hashtag, tweets|
-  		tweets.each do |tweet|
-			message = Message.new
-			message.twitter_id = tweet.id
-			message.author = tweet.user.screen_name
-			message.body = tweet.text
-			message.publication_date = tweet.created_at
-
-			message.save
-
-			hashtag_message = HashtagMessage.new
-			hashtag_message.hashtag_id = hashtag
-			hashtag_message.message_id = message.id
-
-			hashtag_message.save
-		end
-	end
-  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,92 @@ class Message < ApplicationRecord
 	validates :author, presence: true
 	validates :body, presence: true
 	validates :publication_date, presence: true
+
+	require 'twitter'
+
+	def get_twitter_client
+		Twitter::REST::Client.new do |config|
+	    	config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+	    	config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+	    	config.access_token        = ENV['ACCESS_TOKEN']
+	    	config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+		end
+	end
+
+	def get_messages_from_twitter
+		tweets = get_tweets
+		save_tweets(tweets)
+	end
+
+	def get_hashtag_messages_from_twitter(hashtag)
+		client = get_twitter_client
+		tweets = get_tweets_by_hashtag(hashtag.value)
+		save_hashtag_tweets(hashtag.id, tweets)
+	end
+
+	def get_tweets
+  		client = get_twitter_client
+
+		hashtags = Hashtag.all
+		tweets = Hash.new
+
+		hashtags.each do |hashtag|
+			tweets[hashtag.id] = get_tweets_by_hashtag(client, hashtag.value)
+		end
+
+		tweets
+  	end
+
+  	def get_tweets_by_hashtag(client, hashtag)
+  		tweets = client.search("##{hashtag}").take(100)
+  		remove_existing(tweets)
+  	end
+
+  	def remove_existing(tweets)
+  		messages = Message.where(twitter_id: tweets.map(&:id))
+
+  		new_tweets = Array.new
+  		existing_tweets_ids = messages.map(&:twitter_id)
+
+  		tweets.each do |tweet|
+  			unless existing_tweets_ids.include?(tweet.id)
+  				new_tweets.push(tweet)
+  			end
+  		end
+
+  		return new_tweets
+  	end
+
+  	def save_tweets(messages)
+  		messages.each do |hashtag, tweets|
+  			save_hashtag_tweets(hashtag, tweets)
+		end
+  	end
+
+  	def save_hashtag_tweets(hashtag_id, tweets)
+  		tweets.each do |tweet|	
+			message = save_message_from_tweet(tweet)
+			save_hashtag_message(hashtag, message.id)
+		end
+  	end
+
+  	def save_message_from_tweet(tweet)
+  		message = Message.new
+		message.twitter_id = tweet.id
+		message.author = tweet.user.screen_name
+		message.body = tweet.text
+		message.publication_date = tweet.created_at
+
+		message.save
+
+		message
+  	end
+
+  	def save_hashtag_message(hashtag_id, message_id)
+  		hashtag_message = HashtagMessage.new
+		hashtag_message.hashtag_id = hashtag
+		hashtag_message.message_id = message.id
+
+		hashtag_message.save
+  	end
 end
